8b7dcd1ec3335dbfa147b565fb2c9030adb3489b
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-04 11:48:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+	if(!Array.isArray(arr)||arr.length<2){
+		return -Infinity;
+	}
+	
+	let max=-Infinity;
+	let secmax=-Infinity;
+
+	for(let nums of arr){
+		if(num>max){
+			secmax=max;
+			max=num;
+		}else if(num<max && num>secmax){
+			secmax=num;
+		}
+	}
+	  return secmax === -Infinity ? -Infinity : secmax;
+}
+	
+}
